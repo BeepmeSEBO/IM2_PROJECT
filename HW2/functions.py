a00167a1f8696a79e013b6edcf53be7ee6dfd8f1
BeepmeSEBO <sabadoemerson28@gmail.com>
2025-10-01 22:16:07 +0800

    Homework 2

--- HW2/functions.py
+++ HW2/functions.py
@@ -1,21 +1,18 @@
 def char_frequency(text):
     freq = {}
     for char in text:
-        if char != " ":   # ignore spaces
+        if char != " ":
             freq[char] = freq.get(char, 0) + 1
     return freq
 
-# Main program
 user_input = input("Enter string: ")
 
-# Split by comma
 parts = user_input.split(",")
 
 for part in parts:
-    part = part.strip()  # remove extra spaces
+    part = part.strip() 
     result = char_frequency(part)
     
-    # Print output for this part
     for key, value in result.items():
         print(f"{key}={value}", end=" ")
-    print()  # new line for next word
+    print() 
